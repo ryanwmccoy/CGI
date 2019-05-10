1c2a0c9fbdc6c33495c61a2453acbdb4e923be28
Ryan McCoy <rmccoy@CGI.local>
2019-05-10 16:44:59 -0500

    Much progress parsing through the data and pulling out some of the StartDate information

--- Membership_Clean_Files.R
+++ Membership_Clean_Files.R
@@ -1,5 +1,52 @@
 library(dslabs)
 library(dplyr)
+library(ggplot2)
+library(data.table)
 
 
 
+###Breaking the surveyed data into different blocks to be cleansed
+Survey_info <- (initial_data[1:9])
+Personal_info <- (initial_data[10:24])
+Hospital_options <-(initial_data[25:80])
+Survey_questions <-(initial_data[81:121])
+Testimonial_info <- (initial_data[122:127])
+
+
+###Cleaning Block Survey_info
+  ###Starting with StartDate
+Last_survey_started_indexed <- Survey_info$StartDate %>% which.max()
+Last_survey_started <- Survey_info$StartDate[Last_survey_started_indexed]
+
+First_survey_started_indexed <- Survey_info$StartDate %>% which.min()
+First_survey_started <- Survey_info$StartDate[First_survey_started_indexed]
+
+Survey_info.First <- subset(Survey_info,
+                            Survey_info$StartDate >= as.POSIXct(First_survey_started,
+                                                   tz= "UTC") &
+                              Survey_info$StartDate <= as.POSIXct(Last_survey_started,
+                                                   tz = "UTC"))
+head(Survey_info.First[1])
+tail(Survey_info.First[1])
+ls(Survey_info.First)
+class(Survey_info.First$StartDate)
+
+Last_survey_started - First_survey_started
+
+
+
+###write.csv(Survey_info.First,
+###          file = "2019_Membership_Survey_info_Start_times.csv")
+
+  ###Distribution of Start Times###
+Start_time_graph <- Survey_info.First %>% ggplot(aes(Survey_info.First$StartDate)) +
+  ###geom_histogram(binwidth = 31, )
+  geom_density()
+  
+                    
+
+
+
+
+
+Start_time_graph
