12f4f32f2fde9e811bb4d82c90713c2ad9994c02
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2019-05-16 16:34:59 -0500

    Cleaned up the Times - separated out DateTime for analysis

--- Membership_Clean_Files.R
+++ Membership_Clean_Files.R
@@ -39,64 +39,123 @@ Survey_info.First <- subset(Survey_info,
                                                    tz= "UTC") &
                               Survey_info$StartDate <= as.POSIXct(Last_survey_started,
                                                    tz = "UTC"))
-head(Survey_info.First[1])
-tail(Survey_info.First[1])
-ls(Survey_info.First)
-class(Survey_info.First$StartDate)
 
-Last_survey_started - First_survey_started
+SHx <- data.frame (Survey_info.First)
+SHy <- format(as.POSIXct(strptime(Survey_info.First$StartDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%H")
+Start_Hour_reformat<- mutate(SHx,Start_Hour = SHy)#, Start_Minute = Sminy, Start_Year = SYy, Start_Month = Smy, Start_Day = Sdy)
+
+SMinx <- data.frame (Start_Hour_reformat)
+SMiny <- format(as.POSIXct(strptime(Survey_info.First$StartDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%M")
+Start_Min_reformat<- mutate(SMinx,Start_Minute = SMiny)
+
+
+SYx <- data.frame (Start_Min_reformat)
+SYy <- format(as.POSIXct(strptime(Survey_info.First$StartDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%Y")
+Start_Year_reformat<- mutate(SYx,Start_Year = SYy)
+
+
+Smx <- data.frame (Start_Year_reformat)
+Smy <- format(as.POSIXct(strptime(Survey_info.First$StartDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%m")
+Start_Month_reformat<- mutate(Smx,Start_Month = Smy)
+
+
+Sdx <- data.frame (Start_Month_reformat)
+Sdy <- format(as.POSIXct(strptime(Survey_info.First$StartDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%d")
+Start_Day_reformat<- mutate(Sdx,Start_Day = Sdy)
+
+Survey_info.First <- Start_Day_reformat
+
+
+EHx <- data.frame (Survey_info.First)
+EHy <- format(as.POSIXct(strptime(Survey_info.First$EndDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%H")
+End_Hour_reformat<- mutate(EHx,End_Hour = EHy)
+
+EMinx <- data.frame (End_Hour_reformat)
+EMiny <- format(as.POSIXct(strptime(Survey_info.First$EndDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%M")
+End_Min_reformat<- mutate(EMinx,End_Minute = EMiny)
+
+
+EYx <- data.frame (End_Min_reformat)
+EYy <- format(as.POSIXct(strptime(Survey_info.First$EndDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%Y")
+End_Year_reformat<- mutate(EYx,End_Year = EYy)
+
+
+Emx <- data.frame (End_Year_reformat)
+Emy <- format(as.POSIXct(strptime(Survey_info.First$EndDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%m")
+End_Month_reformat<- mutate(Emx,End_Month = Emy)
+
+
+Edx <- data.frame (End_Month_reformat)
+Edy <- format(as.POSIXct(strptime(Survey_info.First$EndDate,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%d")
+End_Day_reformat<- mutate(Edx,End_Day = Edy)
+
+Survey_info.First <- End_Day_reformat
+head(Survey_info.First)
+
+Start_time_graph <- Survey_info.First %>% ggplot(aes(Start_Hour,Duration..in.seconds.)) + geom_point()
 
 
 
+Numeric_Hour <- is.numeric(Survey_info.First$Start_Hour)
+Numberic_Duration <- is.numeric(Survey_info.First$Duration..in.seconds.)
+cor(Numeric_Hour,Numberic_Duration)
+#Average_Start_Hour <- mean(is.numeric(Survey_info.First$Start_Hour), na.rm = TRUE)
+#Average_End_Hour <- mean(is.numeric(Survey_info.First$End_Hour), na.rm = TRUE)
+#Average_Start_Hour
+#Average_End_Hour
+
+#Median_Start_Hour <- Median(is.numeric(Survey_info.First$Start_Hour), na.rm = TRUE)
+#Median_End_Hour <- Median(is.numeric(Survey_info.First$End_Hour), na.rm = TRUE)
+#Median_Start_Hour
+#Median_End_Hour
 ###write.csv(Survey_info.First,
 ###          file = "2019_Membership_Survey_info_Start_times.csv")
 
   ###Distribution of Start Times###
-#Start_time_graph <- Survey_info.First %>% ggplot(aes(Survey_info.First$StartDate)) +
-  ###geom_histogram(binwidth = 31, )
+
 #  geom_density()
 
-#Start_time_graph
+#
 
 
 ###--------------------------------Duration of Survey----------------------------------
-Survey_info$`Duration (in seconds)` <- as.numeric(Survey_info$`Duration (in seconds)`,na.rm= TRUE)
-Average_Duration <- mean(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
-Average_Duration /60
-min(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
-max(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
-median(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
+#Survey_info$`Duration (in seconds)` <- as.numeric(Survey_info$`Duration (in seconds)`,na.rm= TRUE)
+#Average_Duration <- mean(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
+#Average_Duration /60
+#min(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
+#max(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
+#median(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
 
-stdev <- sd(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
-stdev
+#stdev <- sd(Survey_info$`Duration (in seconds)`,na.rm = TRUE)
+#stdev
 
-Q1 <- summary(Survey_info$`Duration (in seconds)`,na.rm = TRUE)[["1st Qu."]]
-Q3 <- summary(Survey_info$`Duration (in seconds)`,na.rm = TRUE)[["3rd Qu."]]
+#Q1 <- summary(Survey_info$`Duration (in seconds)`,na.rm = TRUE)[["1st Qu."]]
+#Q3 <- summary(Survey_info$`Duration (in seconds)`,na.rm = TRUE)[["3rd Qu."]]
 
-Q_upper <- Average_Duration + 1.5*(Q3-Q1)
-Q_lower <- Average_Duration - 1.5*(Q3-Q1)
+#Q_upper <- Average_Duration + 1.5*(Q3-Q1)
+#Q_lower <- Average_Duration - 1.5*(Q3-Q1)
 
-Q_upper
-Q_lower
+#Q_upper
+#Q_lower
 
-Outliers_upper <- Survey_info$`Duration (in seconds)`>=Q_upper
-Outliers_lower <- Survey_info$`Duration (in seconds)`<=Q_lower
+#Outliers_upper <- Survey_info$`Duration (in seconds)`>=Q_upper
+#Outliers_lower <- Survey_info$`Duration (in seconds)`<=Q_lower
 
-boxplot(Survey_info$`Duration (in seconds)`)
-outliers <- boxplot(Survey_info$`Duration (in seconds)`)$out
-print(outliers)
+#boxplot(Survey_info$`Duration (in seconds)`)
+#outliers <- boxplot(Survey_info$`Duration (in seconds)`)$out
+#print(outliers)
 
 ###Survey_info$`Duration (in seconds)`[which(Survey_info$`Duration (in seconds)` %in% outliers),na.rm = TRUE]
 
 
-Outliers2 <- function(x,na.rm = TRUE){
-  x[!x %in% boxplot.stats(x)$out]
-}
-plot(Outliers2(Survey_info$`Duration (in seconds)`))
+#Outliers2 <- function(x,na.rm = TRUE){
+  #x[!x %in% boxplot.stats(x)$out]
+#}
+#plot(Outliers2(Survey_info$`Duration (in seconds)`))
 
 ###Distribution of Durations####
-Duration_graph <- data.frame(Survey_info$`Duration (in seconds)`) %>%
-  ggplot(aes(x = Alternative_Duration)) +
-  geom_histogram(binwidth = 30, na.rm = TRUE)
+#Duration_graph <- data.frame(Survey_info$`Duration (in seconds)`) %>%
+  #ggplot(aes(x = Alternative_Duration)) +
+  #geom_histogram(binwidth = 30, na.rm = TRUE)
 
-Duration_graph
+#Duration_graph
